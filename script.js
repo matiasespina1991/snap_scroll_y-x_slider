d6d6ef63845f6cb902451d329ee4f5e9a5e62ac9
Matias Espina <matiasespina1991@gmail.com>
2021-08-24 00:53:45 +0200

    y slider - custom radio buttons (ready)

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-// document.radioy.radioy.value="radio-btn-y-2";
-
 var parent = document.getElementById('parent');
-var child = document.getElementsByClassName('child');
+var slideY = document.getElementsByClassName('slide-y');
+var slideYlabel = document.getElementsByClassName('slide-y-label');
 var radioBtnY = Array.from(document.getElementsByClassName("radio-btn-y"));
 
 // Set Radio button 1 checked by default
-if (parent.scrollTop == 0) {document.radioy.radioy.value="radio-btn-y-1"};
+if (parent.scrollTop == 0) {
+    slideYlabel[0].style.backgroundColor = "#444"
+};
 
 // Click function for radio button to scroll to the correspondent slide
 radioBtnY.forEach(function(buttonY) {(buttonY.addEventListener('click', (e) => {
-                e.preventDefault();
-                parent.scrollTo({top: child[e.target.dataset.index].offsetTop, behavior: 'smooth'})})
+            e.preventDefault();
+            parent.scrollTo(
+                        {   
+                            top: slideY[e.target.dataset.index].offsetTop,
+                            behavior: 'smooth'
+                        }
+                    );
+                }
             )
-        }
-    )
+        )
+    }
+)
 
-// // Click function for radio button to scroll to the correspondent slide
-// radioBtnY.forEach(function(buttonY) {(buttonY.addEventListener('click', (e) =>
-//             {document.radioy.radioy.value=`radio-btn-y-${e}`})
-//         )
+// Check the label of the radio buttons when scrolling to the correspondent slide
+parent.addEventListener('scroll', () => {
+        if (parent.scrollTop < slideY[1].offsetTop) {slideYlabel[0].style.backgroundColor = "#444"} else {slideYlabel[0].style.backgroundColor = "transparent"};
+        if (parent.scrollTop >= slideY[1].offsetTop && parent.scrollTop < slideY[2].offsetTop) {slideYlabel[1].style.backgroundColor = "#444"} else {slideYlabel[1].style.backgroundColor = "transparent"};
+        if (parent.scrollTop >= slideY[2].offsetTop && parent.scrollTop < slideY[3].offsetTop) {slideYlabel[2].style.backgroundColor = "#444"} else {slideYlabel[2].style.backgroundColor = "transparent"};
+        if (parent.scrollTop >= slideY[3].offsetTop && parent.scrollTop < slideY[4].offsetTop) {slideYlabel[3].style.backgroundColor = "#444"} else {slideYlabel[3].style.backgroundColor = "transparent"};
+        if (parent.scrollTop >= slideY[4].offsetTop) {slideYlabel[4].style.backgroundColor = "#444"} else {slideYlabel[4].style.backgroundColor = "transparent"};
+    }
+)
+
+// Check radio buttons when scrolling to the correspondent slide // ignored
+// parent.addEventListener('scroll', () => {
+//         if (parent.scrollTop < slideY[1].offsetTop) {document.radioy.radioy.value="radio-btn-y-1"};
+//         if (parent.scrollTop >= slideY[1].offsetTop && parent.scrollTop < slideY[2].offsetTop) {document.radioy.radioy.value="radio-btn-y-2"};
+//         if (parent.scrollTop >= slideY[2].offsetTop && parent.scrollTop < slideY[3].offsetTop) {document.radioy.radioy.value="radio-btn-y-3"};
+//         if (parent.scrollTop >= slideY[3].offsetTop && parent.scrollTop < slideY[4].offsetTop) {document.radioy.radioy.value="radio-btn-y-4"};
+//         if (parent.scrollTop >= slideY[4].offsetTop) {document.radioy.radioy.value="radio-btn-y-5"};
 //     }
 // )
-
-parent.addEventListener('scroll', () => {
-    if (parent.scrollTop < child[1].offsetTop) {document.radioy.radioy.value="radio-btn-y-1"};
-    if (parent.scrollTop >= child[1].offsetTop && parent.scrollTop < child[2].offsetTop) {document.radioy.radioy.value="radio-btn-y-2"};
-    if (parent.scrollTop >= child[2].offsetTop && parent.scrollTop < child[3].offsetTop) {document.radioy.radioy.value="radio-btn-y-3"};
-    if (parent.scrollTop >= child[3].offsetTop && parent.scrollTop < child[4].offsetTop) {document.radioy.radioy.value="radio-btn-y-4"};
-    if (parent.scrollTop >= child[4].offsetTop) {document.radioy.radioy.value="radio-btn-y-5"};
-    })
-
-    
-
