5f5ff7b10c8224224a70b0eea42ad0463837421a
Matias Espina <matiasespina1991@gmail.com>
2021-08-24 03:54:43 +0200

    x buttons from first slider ready

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 var parent = document.getElementById('parent');
+var horizontalFirstContainer = document.getElementById('horizontal-first-container');
+var slideX_1 = document.getElementsByClassName('horizontal-1')
 var slideY = document.getElementsByClassName('slide-y');
 var slideYlabel = document.getElementsByClassName('slide-y-label');
+var radioXbtn = document.getElementsByClassName('slide-x-label')
 var radioBtnY = Array.from(document.getElementsByClassName("radio-btn-y"));
 
 // Set Radio button 1 checked by default
 if (parent.scrollTop == 0) {
     slideYlabel[0].style.backgroundColor = "#444"
+};
+
+if (horizontalFirstContainer.scrollLeft == 0) {
+    radioXbtn[0].style.backgroundColor = "#444";
 };
 
 // Click function for radio button to scroll to the correspondent slide
@@ -25,12 +32,22 @@ radioBtnY.forEach(function(buttonY) {(buttonY.addEventListener('click', (e) => {
 
 // Check the label of the radio buttons when scrolling to the correspondent slide
 parent.addEventListener('scroll', () => {
-        if (parent.scrollTop < slideY[1].offsetTop) {slideYlabel[0].style.backgroundColor = "#444"} else {slideYlabel[0].style.backgroundColor = "transparent"};
+        if (parent.scrollTop < slideY[1].offsetTop) {slideYlabel[0].style.backgroundColor = "#444"; radioXbtn[0].style.display = "flex"} else {slideYlabel[0].style.backgroundColor = "transparent";};
         if (parent.scrollTop >= slideY[1].offsetTop && parent.scrollTop < slideY[2].offsetTop) {slideYlabel[1].style.backgroundColor = "#444"} else {slideYlabel[1].style.backgroundColor = "transparent"};
         if (parent.scrollTop >= slideY[2].offsetTop && parent.scrollTop < slideY[3].offsetTop) {slideYlabel[2].style.backgroundColor = "#444"} else {slideYlabel[2].style.backgroundColor = "transparent"};
         if (parent.scrollTop >= slideY[3].offsetTop && parent.scrollTop < slideY[4].offsetTop) {slideYlabel[3].style.backgroundColor = "#444"} else {slideYlabel[3].style.backgroundColor = "transparent"};
         if (parent.scrollTop >= slideY[4].offsetTop) {slideYlabel[4].style.backgroundColor = "#444"} else {slideYlabel[4].style.backgroundColor = "transparent"};
     }
+)
+
+horizontalFirstContainer.addEventListener('scroll', () => {
+        if (horizontalFirstContainer.scrollLeft < slideX_1[1].offsetLeft) {radioXbtn[0].style.backgroundColor = "#444"; radioXbtn[1].style.backgroundColor = "transparent";} else {radioXbtn[0].style.backgroundColor = "transparent"; radioXbtn[1].style.backgroundColor = "#444";};
+    }
+)
+
+parent.addEventListener('scroll', () => {
+    if (parent.scrollTop < slideY[1].offsetTop) {radioXbtn[0].style.display = "flex"; radioXbtn[1].style.display = "flex"} else {radioXbtn[0].style.display = "none"; radioXbtn[1].style.display = "none"};
+}
 )
 
 // Check radio buttons when scrolling to the correspondent slide // ignored
